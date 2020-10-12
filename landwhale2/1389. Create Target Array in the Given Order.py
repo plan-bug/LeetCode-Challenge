530c99ad4165b290ed012e028f5e70e0b682eece
HyeokJin Kim <landwhale@kakao.com>
2020-10-12 14:07:03 +0900

    Create 1389. Create Target Array in the Given Order.py

---
+++ landwhale2/1389. Create Target Array in the Given Order.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+        for n,i in zip(nums, index):
+            target.insert(i,n)
+        return target
