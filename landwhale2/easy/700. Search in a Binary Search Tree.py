85cadc0a10a93327d15357b06703a95071338d2f
HyeokJin Kim <landwhale@kakao.com>
2020-10-16 16:20:14 +0900

    Create 700. Search in a Binary Search Tree.py

---
+++ landwhale2/easy/700. Search in a Binary Search Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        elif root.val < val:
+            return self.searchBST(root.right, val)
