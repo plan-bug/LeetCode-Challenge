18f6d0e3028e5d39fd5534a06a76cb1c8cee8550
허석호 <microcephalus7@gmail.com>
2020-10-16 15:32:29 +0900

    10월 16일

--- microcephalus7/greedy/860.my.py
+++ microcephalus7/greedy/860.my.py
@@ -4,6 +4,7 @@
 # 쌓이는 돈 차액으로 뺌
 # 쌓이는 돈 음수값 될 시 False
 # 쌓이는 돈 양수값 일 시 True
+# 5달러
 
 
 from typing import List
@@ -11,4 +12,21 @@ from typing import List
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        return bills
+        rest = 0
+        for i in bills:
+            if i == 5:
+                rest += i
+            else:
+                rest -= (i-5)
+                if rest < 0:
+                    return False
+                rest += i
+        return rest
+
+
+solution = Solution()
+print(solution.lemonadeChange([5, 5, 5, 10, 20]))
+print(solution.lemonadeChange([5, 5, 10]))
+print(solution.lemonadeChange([10, 10]))
+print(solution.lemonadeChange([5, 5, 10, ]))
+print(solution.lemonadeChange([5, 10, 5, 20]))
