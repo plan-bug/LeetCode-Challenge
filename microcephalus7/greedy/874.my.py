bcbf7df83bc099835c6c853edf26fb332ff25fe5
허석호 <microcephalus7@gmail.com>
2020-10-22 11:45:06 +0900

    10월 22일

--- microcephalus7/greedy/874.my.py
+++ microcephalus7/greedy/874.my.py
@@ -1,26 +1,29 @@
 # for 문
 # command 에서 i 값이 -1, -2 이냐에 따라 다음 좌표값 바뀜(우선 처리 필요)
 # obstacles 값에 따른 처리 필요 (더하고 난 뒤 처리.)
+# 튜플로 처리 필요
 from typing import List
 
 
 class Solution:
     def robotSim(self, commands: List[int], obstacles: [List[List[int]]]) -> int:
-        x, y = 0, 0
-        direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        number = 0
-        for i in commands:
-            if i == -1:
+        x, y, number, maxDistance = 0, 0, 0, 0
+        direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        for command in commands:
+            if command == -1:
                 number = (number+1) % 4
-            elif i == -2:
-                number -= 1
-            else:
+            elif command == -2:
                 number = (number-1) % 4
-                for j in range(i):
-                    if [x+direction[number][0], y+direction[number][1]] not in obstacles:
-                        x += direction[number][0]
-                        y += direction[number][1]
-        return (x**2)+(y**2)
+            else:
+                xOff, yOff = direction[number]
+                while command:
+                    if [x + xOff, y + yOff] not in obstacles:
+                        x += xOff
+                        y += yOff
+                    command -= 1
+                maxDistance = max(maxDistance, x**2 + y**2)
+        print(x, y)
+        return maxDistance
 
 
 solution = Solution()
