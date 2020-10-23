b3eba4100d920e12a6af493391316ef15ec17d0d
microcephalus7 <microcephalus7@gmail.com>
2020-10-23 23:26:06 +0900

    dd

--- microcephalus7/sorted/1528.my.py
+++ microcephalus7/sorted/1528.my.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
-    def restoreString(self, s: str, indices: List[int]) -> str:
-        strings = [(sValue, index) for sValue, index in zip(list(s), indices)]
+    def restoreString(self, string: str, indices: List[int]) -> str:
+        strings = [(sValue, index)
+                   for sValue, index in zip(list(string), indices)]
         return ''.join([sval[0] for sval in sorted(strings, key=lambda x: x[1])])
