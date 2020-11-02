b0cd5938779ec8de7698c0ff13803a4b4dc62a6d
허석호 <microcephalus7@gmail.com>
2020-11-02 21:51:46 +0900

    1030

--- microcephalus7/sorted/1030.my.py
+++ microcephalus7/sorted/1030.my.py
@@ -1,3 +1,10 @@
+from typing import List
+
+
 class Solution:
     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
-        return (r0, c0)
+        return sorted([[i, j] for i in range(R) for j in range(C)], key=lambda x: abs(x[0]-r0)+abs(x[1]-c0))
+
+
+solution = Solution()
+print(solution.allCellsDistOrder(3, 3, 0, 0))
