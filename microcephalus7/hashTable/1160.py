670851f55974386b67d45b334dab41e85c00bafc
microcephalus7 <microcephalus7@gmail.com>
2020-11-17 21:31:15 +0900

    1160

---
+++ microcephalus7/hashTable/1160.py
@@ -0,0 +1,11 @@
+# words 엘리먼트로 Counter 생성
+# 엘리먼트의 인덱스 값과 chars 의 index 값 이터레이션 하며 크기 비교
+# 모든 요소가 chars Counter 보다 작을 때 list 삽입
+
+
+from typing import List
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        return words
