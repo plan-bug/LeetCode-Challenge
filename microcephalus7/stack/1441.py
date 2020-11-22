28c1866c2071c2d41e4ddb757acb4e4983f42ade
microcephalus7 <microcephalus7@gmail.com>
2020-11-22 10:09:27 +0900

    682

--- microcephalus7/stack/1441.py
+++ microcephalus7/stack/1441.py
@@ -1,3 +1,10 @@
+# 투 포인터 같은 개념
+# 1을 기준으로 각 이터러블
+# 1이 이터러블 보다 작으면 push, pop
+# 같아졌을 때 push
+
+# 반복해야할 땐 for, while
+
 from typing import List
 
 
@@ -5,7 +12,6 @@ class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         operations = []
         currentNum = 1
-
         for num in target:
             while currentNum < num:
                 operations.append("Push")
---
+++ microcephalus7/stack/682.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        points = []
+        for i in ops:
+            if i == "+":
+                points.append(sum(points[-2:]))
+            elif i == 'D':
+                points.append(2*points[-1])
+            elif i == "C":
+                points.pop()
+            else:
+                points.append(int(i))
+        return sum(points)
+
+
+solution = Solution()
+print(solution.calPoints([]))
