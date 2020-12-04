476512b3a9ce44bf941988c3b63ea73a6dfca880
microcephalus7 <microcephalus7@gmail.com>
2020-12-04 20:24:01 +0900

    690 added comment

--- microcephalus7/BFS/690.py
+++ microcephalus7/BFS/690.py
@@ -4,6 +4,13 @@
 # 해당 id importance 값 추출 및 해당 subordinates 재 서치
 # 해당 id 값들 다시 서치 후 value 추출
 
+# 정답 sol
+
+# employees 값에서 id가 key 이고 값이 employees 인 dictonary 생성
+# 객체에서 해당 id 값 가진 stack 생성
+# stack에서 pop 으로 제거 및 추출 후 해당 값에서 importance 추출 및 subortinates 값 stack 에 삽입 반복
+# totalImp return
+
 class Employee:
     def __init__(self, id: int, importance: int, subordinates: List[int]):
         self.id = id
