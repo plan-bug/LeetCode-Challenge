979ff31eef5db575d7373098aaf59834ee4963a6
microcephalus7 <microcephalus7@gmail.com>
2021-01-20 21:38:48 +0900

    1379 plus comment

--- microcephalus7/Tree/1379.py
+++ microcephalus7/Tree/1379.py
@@ -1,4 +1,6 @@
-
+# or 의 이유
+# 처음의 if not original 일때 return 이 None이기 때문
+# 어느 쪽에서든 node가 none이 되면 다음 함수로 넘어가는 것이 아니라 그 순간 None이 되고 함수가 종료되기 때문에 or을 통해 True가 있을 때까지 감
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         if not original:
