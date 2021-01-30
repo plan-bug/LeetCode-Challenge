1a85efe6b0b23b5bf4bd1b682b3615a347fc6b2e
microcephalus7 <microcephalus7@gmail.com>
2021-01-30 09:53:23 +0900

    1480

--- microcephalus7/easy/1480.py
+++ microcephalus7/easy/1480.py
@@ -1,8 +1,8 @@
 # enumrate 이용
-
+# List 의 index 를 잘 이용하느냐
 class Solution:
     def runningSum(self,nums:List[int]) -> List[int]:
         newList = []
-        for idx, val in enumerate(nums):
-            newList.append(sum(nums[0:idx+1]))
+        for i in range(len(nums)):
+            newList.append(sum(nums[0:i+1]))
         return newList
