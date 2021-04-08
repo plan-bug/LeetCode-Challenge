72a3f812c493c67c1584e20e32a09a38ceb8ce5c
microcephalus7 <microcephalus7@gmail.com>
2021-04-08 21:49:08 +0900

    54

---
+++ microcephalus7/medium/654.py
@@ -0,0 +1,11 @@
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if not nums :
+            return None
+        maxNum = max(nums)
+        idx = nums.index(maxNum)
+        node = TreeNode(maxNum)
+        node.left = self.constructMaximumBinaryTree(nums[:idx])
+        node.right = self.constructMaximumBinaryTree(nums[idx+1:])
+        return node
+        
