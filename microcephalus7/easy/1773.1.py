51cf6c3b0aae3fb9df83681cea5475f9defbca1a
microcephalus7 <microcephalus7@gmail.com>
2021-04-13 21:29:49 +0900

    1773.1

--- microcephalus7/easy/1773.1.py
+++ microcephalus7/easy/1773.1.py
@@ -1,3 +1,3 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        return sum (1 for t,c,n in items if (ruleKey,ruleValue) in (("type",t), ("color",c), ("name",n)))
+        return len([0 for t,c,n in items if (ruleKey,ruleValue) in (("type",t),("color",c),("name",n))])
