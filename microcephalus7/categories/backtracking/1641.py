cbb6ba3c1ef1586cb78a95ef6c651eed6e47581b
microcephalus7 <microcephalus7@gmail.com>
2021-05-04 20:25:28 +0900

    1641

---
+++ microcephalus7/categories/backtracking/1641.py
@@ -0,0 +1,8 @@
+# 알고리즘 주제가 정해져있다고 해서 그 방법에 한정될 필요는 없음
+# 조합으로 풀 수도 있음
+
+import itertools
+
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        return len(list(itertools.combinations_with_replacement(range(5),n)))
