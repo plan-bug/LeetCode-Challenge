d6fbc232730c877d0db808276e043e2eb9beef69
microcephalus7 <microcephalus7@gmail.com>
2021-05-20 20:07:25 +0900

    Add 230

---
+++ microcephalus7/medium/230.py
@@ -0,0 +1,14 @@
+# dfs/bfs 로 arr(list) node val 모음
+#  arr 정렬 후 arr[k] return
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        res= []
+        stack = [root]
+        while stack:
+            newNode = stack.pop()
+            if newNode:
+                res.append(newNode.val)
+                stack.append(newNode.left)
+                stack.append(newNode.right)
+        return sorted(res)[k-1]
