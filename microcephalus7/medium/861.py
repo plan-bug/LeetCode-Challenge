9addf87b1e7cf75dc93a475a6e1077b05c7fd81b
microcephalus7 <microcephalus7@gmail.com>
2021-06-07 20:36:32 +0900

    Create 861

---
+++ microcephalus7/medium/861.py
@@ -0,0 +1,14 @@
+# 1 을 어떻게 다룰 것이냐
+
+# 필요 조건
+# grid(2중 List) 개념 및 다루기
+# 언제 row, column 의 전환할 것이냐
+
+# 숙지 사항
+
+from typing import List
+
+
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        return grid
