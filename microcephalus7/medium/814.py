b544669ff45ba1b5f3b56b072958567aac5fd018
microcephalus7 <microcephalus7@gmail.com>
2021-06-24 21:01:17 +0900

    Create 814

---
+++ microcephalus7/medium/814.py
@@ -0,0 +1,13 @@
+# BackTracking?
+# 1이 나오지 않은 node 는 None return?
+
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and not root.val:
+            return None
+        return root
